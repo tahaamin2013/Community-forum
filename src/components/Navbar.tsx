63afb1c029a8100374d487d5ff9d34ec4851671e
tahaamin2013 <tahaamindob2013@gmail.com>
2023-12-15 19:05:21 +0500

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,6 @@ const Navbar = async () => {
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-white shadow-md z-[10] py-2'>
     <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
-      {/* logo */}
       <Link href='/' className='flex gap-2 items-center'>
         <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
         <p className='hidden text-zinc-700 text-sm font-medium md:block'>Breadit</p>
