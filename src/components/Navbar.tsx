e03822922158de4a38543678060d9d26dbe28f30
tahaamin2013 <tahaamindob2013@gmail.com>
2023-12-16 17:41:28 +0500

    fsda

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,54 +1,31 @@
-"use client"
-import Link from 'next/link';
-import React from 'react';
-import { Icons } from './icons';
-import { Button, buttonVariants } from './ui/Button';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import Link from 'next/link'
+import React from 'react'
+import { Icons } from './icons'
+import { buttonVariants } from './ui/Button'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
 
-const Navbar = () => {
-  const { data: session } = useSession();
-  const [isLoggedIn, setIsLoggedIn] = React.useState(!!session);
-
-  const handleSignIn = async () => {
-    try {
-      const signInResult = await signIn('github');
-      if (signInResult?.error) {
-        console.error(signInResult.error);
-      } else {
-        setIsLoggedIn(true); // Update isLoggedIn state immediately after successful login
-      }
-    } catch (error) {
-      console.error('Error during sign-in:', error);
-    }
-  };
-
-  const handleSignOut = async () => {
-    try {
-      await signOut();
-      setIsLoggedIn(false); // Update isLoggedIn state immediately after successful logout
-    } catch (error) {
-      console.error('Error during sign-out:', error);
-    }
-  };
-
+const Navbar = async () => {
+  const session = await getServerSession(authOptions)
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-white shadow-md z-[10] py-2'>
-      <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
-        <Link href='/' className='flex gap-2 items-center'>
-          <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
-          <p className='hidden text-zinc-700 text-sm font-medium md:block'>Breadit</p>
-        </Link>
+    <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
+      {/* logo */}
+      <Link href='/' className='flex gap-2 items-center'>
+        <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
+        <p className='hidden text-zinc-700 text-sm font-medium md:block'>Breadit</p>
+      </Link>
 
-        {isLoggedIn ? (
-          <p>You are logged in</p>
+      {session?.user ? (
+          <p>youre logged in</p>
         ) : (
-          <Button onClick={session !== null ? handleSignOut : handleSignIn}>
-            Login
-          </Button>
+          <Link href='/sign-in' className={buttonVariants()}>
+            Sign In
+          </Link>
         )}
       </div>
-    </div>
-  );
-};
+      </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
